dfba93c52ca15544924fce2402071bf2b7d759dc
sumitchawla18 <sumit_chawla18@hotmail.com>
2025-06-20 13:05:50 -0400

    Concatination of strings
    Topic covered:
    how to concatenate strings with different data types.

---
+++ concat.py
@@ -0,0 +1,18 @@
+'''age = int (input("Enter age: "))
+name = input ("Enter your name: ")
+sub1 = float (input ("Enter your marks :"))
+sub2 = float (input ("Enter your marks :"))
+sub3 = float (input ("Enter your marks :"))
+sub4 = float (input ("Enter your marks: "))
+calc = (sub1+sub2+sub3+sub4)/4
+print("your name is "+name + " and your age is "+str(age) + " and your avg is "+str(calc))'''
+
+#Program to calculate Salary with Bonus
+
+name = input ("Enter your name: ")
+age = int (input("Enter age: "))
+sal = float (input ("Enter your Salary :"))
+bon = sal*0.1
+Mon = sal + bon
+print("Your name is "+name + " and your age is "+str(age)+" and your salary is "+str(sal)+" and your monthly salary with bonus is "+str(Mon))
+
