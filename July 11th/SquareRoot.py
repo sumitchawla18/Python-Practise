d11e907a3e413f95c81e6d9420037f0ebc95819d
sumitchawla18 <sumit_chawla18@hotmail.com>
2025-07-11 23:16:29 -0400

    square root

---
+++ July 11th/SquareRoot.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number for which you want Square root: "))
+f = 1
+#def sqRoot(n):
+    
+for i in range(0,n):
+    f = i*i
+    print (f"Square Root of numbers before {n} are",f)
+      
+    #return f
+
+
+
